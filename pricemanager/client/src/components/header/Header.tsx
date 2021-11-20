5819a88ee283798b4a36ffad8b3b26851408a326
paulloy <paulloy020896@gmail.com>
2021-11-20 13:34:23 +0000

    Style basic header

--- pricemanager/client/src/components/header/Header.tsx
+++ pricemanager/client/src/components/header/Header.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getPapers } from '../../actions/papers';
+import React from 'react';
 
 function Header() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPapers());
-    }, [])
-
     return (
-        <h1 className="bg-blue-400">RISER WORLD</h1>
+        <header className="flex flex-row h-16 bg-blue-100">
+            <div className="flex items-center">
+                <h1 className="text-2xl p-3.5 text-blue-800">PictureCraft Price Manager</h1>
+            </div>
+            <nav className="flex items-center">
+                <ul className="flex flex-row p-3.5">
+                    <li className="ml-3.5 text-1"><a className="text-blue-800">Printing</a></li>
+                    <li className="ml-3.5 text-1"><a className="text-blue-800">Canvas</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
