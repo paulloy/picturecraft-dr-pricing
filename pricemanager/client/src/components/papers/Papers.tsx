69c9fb8929c44cb36a5b8af06e29ebdd31a7b835
paulloy <paulloy020896@gmail.com>
2021-11-20 21:33:23 +0000

    Add Order details and function for calculating totals

--- pricemanager/client/src/components/papers/Papers.tsx
+++ pricemanager/client/src/components/papers/Papers.tsx
@@ -22,10 +22,79 @@ export default function Papers() {
     const [lengthUnit, setLengthUnit] = useState('inches');
     const [lengthInputs, setLengthInputs] = useState({
         width: 0,
-        length: 0
+        length: 0,
+        qty: 1
     });
 
     const [selectedPaper, setSelectedPaper] = useState('');
+
+    const [totals, setTotals] = useState({
+        netTotal: 0,
+        vat: 0,
+        discountPercentage: 0,
+        discount: 0,
+        subTotal: 0,
+        total: 0
+    });
+
+    const convert = require('convert-length');
+
+    const calculateTotals = () => {
+        // return null if no paper is selected
+        if (selectedPaper.length === 0) return null;
+
+        let { width, length, qty } = lengthInputs;
+        // Cost of paper is the price of a 16" x 20" print
+        const standardArea = 16 * 20;
+        const costPerUnitArea = (selectedPaper[0].cost / 1.2 ) / standardArea; 
+
+        let discountPercentage;
+        // Discount is dependant on quantity
+        if (qty >= 50) {
+            discountPercentage = 0.2
+        } else if (qty >= 20) {
+            discountPercentage = 0.1;
+        } else if (qty >= 10) {
+            discountPercentage = 0.05;
+        } else {
+            discountPercentage = 0;
+        }
+
+        // if selected, convert cm into inches
+        if (lengthUnit === 'cm') {
+            width = convert(width, 'cm', 'in');
+            length = convert(length, 'cm', 'in');
+        }
+
+        // Net Total = Product of the image area (width * length) and costPerUnitArea and quantity
+        let netTotal = (Math.round(((width * length) * costPerUnitArea) * 100) / 100) * qty;
+
+        
+        let discount;
+        if (discountPercentage !== 0) {
+            discount = Math.round((netTotal * discountPercentage) * 100) / 100;
+        } else {
+            discount = 0;
+        }
+        discountPercentage = discountPercentage * 100;
+
+        let vat = Math.round(((netTotal - discount) * 0.2) * 100) / 100;
+        let subTotal = (netTotal - discount) + vat;
+        let total = subTotal;
+
+        setTotals({
+            netTotal: netTotal,
+            vat: vat,
+            discountPercentage: discountPercentage,
+            discount: discount,
+            subTotal: subTotal,
+            total: total
+        });
+    }
+
+    useEffect(() => {
+        calculateTotals();
+    }, [selectedPaper, lengthInputs, lengthUnit]);
 
     return (
         <>
@@ -36,7 +105,7 @@ export default function Papers() {
                     className="block mb-3 border border-black" 
                     onChange={(e: ChangeEvent<HTMLSelectElement>) => {
                             setLengthUnit(e.target.value);
-                            setLengthInputs({ width: 0, length: 0 });
+                            setLengthInputs({ width: 0, length: 0,  qty: 1 });
                         }}>
                     <option value="inches">inches</option>
                     <option value="cm">cm</option>
@@ -67,6 +136,18 @@ export default function Papers() {
                         onBlur={ (e: FocusEvent<HTMLInputElement>) => setLengthInputs({ ...lengthInputs, length: ((Math.round(e.target.valueAsNumber * 100)) / 100) }) }/>
                     <span className="ml-3">{ lengthUnit }</span>
                 </span>
+                <span className="block mb-3 p-3 border border-black">
+                    <label>Quantity</label>
+                    <input 
+                        className="border ml-3 border-black text-right" 
+                        type="number" 
+                        step="1" 
+                        min="1" 
+                        max="1000" 
+                        value={ lengthInputs.qty }
+                        onChange={ (e: ChangeEvent<HTMLInputElement>) => setLengthInputs({ ...lengthInputs, qty: e.target.valueAsNumber }) }
+                        onBlur={ (e: FocusEvent<HTMLInputElement>) => setLengthInputs({ ...lengthInputs, qty: Math.round(e.target.valueAsNumber) }) }/>
+                </span>
             </div>
             {/* /IMAGE DIMENSIONS */}
             {/* PAPER SELECTOR */}
@@ -79,6 +160,50 @@ export default function Papers() {
                 </select>
             </div>
             {/* /PAPER SELECTOR */}
+            {/* ORDER DETAILS */}
+            <div className="border border-black p-3 flex flex-col items-center">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Width</td>
+                            <td>{ lengthInputs.width } { lengthUnit }</td>
+                        </tr>
+                        <tr>
+                            <td>Length</td>
+                            <td>{ lengthInputs.length } { lengthUnit }</td>
+                        </tr>
+                        <tr>
+                            <td>Paper Type</td>
+                            <td>{ selectedPaper.length === 0 ? 'Please select a paper' : selectedPaper[0].name }</td>
+                        </tr>
+                        <tr>
+                            <td>Quantity</td>
+                            <td>{ lengthInputs.qty }</td>
+                        </tr>
+                        <tr>
+                            <td>Net Total</td>
+                            <td>£{ totals.netTotal.toFixed(2) }</td>
+                        </tr>
+                        <tr>
+                            <td>VAT</td>
+                            <td>£{ totals.vat.toFixed(2) }</td>
+                        </tr>
+                        <tr>
+                            <td>Discount</td>
+                            <td><span className="text-red-600 mr-2">{ totals.discountPercentage }%</span>£{ totals.discount.toFixed(2) }</td>
+                        </tr>
+                        <tr>
+                            <td>Sub Total</td>
+                            <td>£{ totals.subTotal.toFixed(2) }</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>£{ totals.total.toFixed(2) }</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            {/* /ORDER DETAILS */}
         </>
     );
 }
