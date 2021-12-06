c413cd9d21406d75a1b198dc188543423200c8ff
paulloy <paulloy020896@gmail.com>
2021-12-06 21:08:05 +0000

    Fix: syntax error

--- pricemanager/accounts/urls.py
+++ pricemanager/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
-urlpattern = [
+urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
