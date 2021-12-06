1b032b7e247107cdb432bb47ff0f5fbf73e8a38d
paulloy <paulloy020896@gmail.com>
2021-12-06 22:01:20 +0000

    User can successfully login to their account

--- pricemanager/client/src/components/accounts/Login.tsx
+++ pricemanager/client/src/components/accounts/Login.tsx
@@ -1,6 +1,14 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
+import { loginUser } from '../../actions/auth';
+import { RootState } from '../../reducers';
 
 export default function Login() {
+    const auth = useSelector((state: RootState) => state.auth);
+
+    const dispatch = useDispatch();
+
     const [form, setForm] = useState({
         username: '',
         password: ''
@@ -14,9 +22,17 @@ export default function Login() {
         });
     }
 
+    const onFormSubmission = (e: FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { username, password } = form;
+        dispatch(loginUser(username, password));
+    }
+
+    if (auth.isAuthenticated) return <Navigate to='/' />;
+
     return (
         <>
-            <form className='relative z-10 p-3 bg-green-100 w-96 mx-auto mt-5'>
+            <form className='relative z-10 p-3 bg-green-100 w-96 mx-auto mt-5' onSubmit={(e) => onFormSubmission(e)}>
                 <h2 className='text-xl text-center mb-3'>Login</h2>
                 <div className='grid grid-cols-2 gap-5'>
                     <label className='text-lg text-right'>Username</label>
