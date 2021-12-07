637e734087f1c05e8f6adde4086e060db7c039ba
paulloy <paulloy020896@gmail.com>
2021-12-07 19:33:46 +0000

    Add private and public links functionality

--- pricemanager/client/src/components/header/Header.tsx
+++ pricemanager/client/src/components/header/Header.tsx
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { RootState } from '../../reducers';
 
 function Header() {
+
+    const auth = useSelector((state: RootState) => state.auth.isAuthenticated);
+
+    const authLinks = (
+        <>
+            <li className="ml-3.5 text-1">
+                <Link to='/'>Printing</Link>
+            </li>
+            <li className="ml-3.5 text-1">
+                <Link to='/cart'><i className="fas fa-shopping-cart"></i></Link>
+            </li>
+            <li className="ml-3.5 text-1">
+                <Link to='/accounts/logout'>LOGOUT</Link>
+            </li>
+        </>
+    );
+
+    const publicLinks = (
+        <>
+            <li className="ml-3.5 text-1">
+                <Link to='/accounts/login'>Login</Link>
+            </li>
+        </>
+    )
 
     return (
         <>
@@ -11,16 +37,7 @@ function Header() {
                 </div>
                 <nav className="flex items-center">
                     <ul className="flex flex-row text-lg p-3.5">
-                        <li className="ml-3.5 text-1">
-                            <Link to='/'>Printing</Link>
-                        </li>
-                        {/* <li className="ml-3.5 text-1"><a className="text-blue-800">Canvas</a></li> */}
-                        <li className="ml-3.5 text-1">
-                            <Link to='/cart'><i className="fas fa-shopping-cart"></i></Link>
-                        </li>
-                        {/* <li className="ml-3.5 text-1">
-                            <Link to='/order-pdf'>PDF</Link>
-                        </li> */}
+                        { auth ? authLinks : publicLinks }
                     </ul>
                 </nav>
             </header>
