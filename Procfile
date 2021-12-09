d936e581999f87cf7259fedd8bcce1132aeb406b
paulloy <paulloy020896@gmail.com>
2021-12-09 22:43:32 +0000

    Prep project for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pricemanager.wsgi:application
--- pricemanager/pricemanager/settings.py
+++ pricemanager/pricemanager/settings.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 
 env = environ.Env()
@@ -18,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['picturecraftpricemanager.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -75,12 +77,17 @@ WSGI_APPLICATION = 'pricemanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
