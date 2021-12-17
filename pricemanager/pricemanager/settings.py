7061c745adcb532c30bb8c595728c22fa74e9518
paulloy <paulloy020896@gmail.com>
2021-12-17 23:06:31 +0000

    Update settings

--- pricemanager/pricemanager/settings.py
+++ pricemanager/pricemanager/settings.py
@@ -1,11 +1,7 @@
 import os
 from pathlib import Path
-import environ
 import dj_database_url
 
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
